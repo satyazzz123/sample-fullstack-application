3bc54a47fbd9d96bcac29f8f70a39ea928f7f4e1
Karun Agarwal <singhalkarun@gmail.com>
2024-06-07 15:55:57 +0530

    add redis based cache module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './modules/todo/todo.module';
 import { PrismaModule } from './global/prisma/prisma.module';
+import * as redisStore from 'cache-manager-redis-store';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
     PrismaModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      url: process.env.REDIS_URL
+    }),
     TodoModule],
   controllers: [AppController],
   providers: [AppService],
